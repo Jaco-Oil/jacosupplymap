ade4e7c92a10c22670b0c8de64d0465d9bb4793b
Viktor Lototskyi <viktorl@starstechnologies.com>
2020-04-29 13:01:01 -0400

    Fix AM to PM for cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('ext:import')
-                  ->dailyAt('07:00');;
+                  ->dailyAt('19:00');;
     }
 
     /**
