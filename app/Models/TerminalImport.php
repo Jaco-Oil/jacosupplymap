b2e0ecb7284a72a0a51fafa2ee41be872969d49d
Viktor Lototskyi <viktorl@starstechnologies.com>
2020-05-27 01:46:32 -0400

    Update terminal view

--- app/Models/TerminalImport.php
+++ app/Models/TerminalImport.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class TerminalImport extends Model
 {
     protected $connection = 'sqlsrv';
-    protected $table='wfifuel.dbo.vw_WF_Terminal';
+    protected $table='wfifuel.bbm.vBestBuyMapTerminal';
 }
