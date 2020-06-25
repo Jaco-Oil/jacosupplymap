535e45e85036ae3ea79d557acefdc6e4574db4cd
Viktor Lototskyi <viktorl@starstechnologies.com>
2020-06-24 20:17:20 -0400

    Color changes and new offset stations

--- public/js/core.js
+++ public/js/core.js
@@ -51,7 +51,7 @@ var JacoOilMap = (function () {
             "PREMIUM 91": "PREMIUM",
             "CLEAR CARB DIESEL 2": "CLEAR CARB DIESEL"
         };
-        var offsetStations = [3366, 3340, 7707, 7775, 8891, 9920, 9925, 9906];
+        var offsetStations = [3366, 3340, 7707, 7775, 8891, 9920, 9925, 9906, 5560, 8890, 7781, 9922, 9910];
 
         function init() {
             _setElements();
@@ -178,7 +178,8 @@ var JacoOilMap = (function () {
                             if (feature.getProperty('icon')) {
                                 mapObject.data.setStyle(function(feature) {
                                     return {
-                                        fillColor: 'green',
+                                        fillColor: 'orange',
+                                        fillOpacity: 0.2,
                                         strokeWeight: 1,
                                         clickable: true,
                                         icon: "images/factory-pin.png"
